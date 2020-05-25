dedfe39805ccfd60cf6034cb9ed9ee682f8da623
Maskz1 <o4d0b@outlook.com>
2020-05-24 18:21:28 -0700

    Added profiles viewset and permissions

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,8 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
 
 
 class HelloApiView(APIView):
@@ -19,7 +22,7 @@ class HelloApiView(APIView):
             'Gives you the most control over your application logic',
             'Is mapped manually to URLs',
         ]
-        
+
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
 
     def post(self, request):
@@ -29,18 +32,18 @@ class HelloApiView(APIView):
         if serializer.is_valid():
             name = serializer.validated_data.get('name')
             message = f'Hello {name}'
-            # By default Response returns a 200 ok 
+            # By default Response returns a 200 ok
             return Response({'message': message})
-        
+
         else:
             return Response(
                 serializer.errors, status=status.HTTP_400_BAD_REQUEST
             )
-    
+
     def put(self, request, pk=None):
         """Handle updating an object"""
         return Response({'method': 'PUT'})
-    
+
     def patch(self, request, pk=None):
         """Handle a partial update of an object"""
         return Response({'method': 'PATCH'})
@@ -63,9 +66,8 @@ class HelloViewSet(viewsets.ViewSet):
             'Provides more functionality with less code',
         ]
 
-
         return Response({'message': 'Hello!', 'a_viweset': a_viewset})
-    
+
     def create(self, request):
         """Create a new Hello message"""
         serializer = self.serializer_class(data=request.data)
@@ -79,7 +81,7 @@ class HelloViewSet(viewsets.ViewSet):
                 serializer.errors,
                 status=status.HTTP_400_BAD_REQUEST
             )
-    
+
     def retrieve(self, request, pk=None):
         """Handle getting an object by its ID"""
         return Response({'http_method': 'GET'})
@@ -87,11 +89,19 @@ class HelloViewSet(viewsets.ViewSet):
     def update(self, request, pk=None):
         """Handle updating an object"""
         return Response({'http_method': 'PUT'})
-    
+
     def partial_update(self, request, pk=None):
         """Handle updating part of an object"""
         return Response({'http_method': 'PATCH'})
-    
+
     def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (permissions.UpdateOwnProfile, )
